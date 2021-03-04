cf03febd265eb6afabb4123f2b52d804c5d448a6
Juan Camilo Moreno <juancamilomg21@gmail.com>
2021-03-04 09:08:47 -0500

    Line Deleted

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import ChallengeDescription from "@components/molecules/ChallengeDescription/ChallengeDescription";
-import Navbar from "@components/molecules/Navbar/Navbar";
-import Button from '../styles/components/atoms/Button/Button';
+import ChallengeDescription from '@components/molecules/ChallengeDescription/ChallengeDescription';
+import Navbar from '@components/molecules/Navbar/Navbar';
 
 const Landing = () => {
   return (
-    <div className="flex justify-center flex-wrap">
+    <div className='flex justify-center flex-wrap'>
       <Navbar />
-      <h1 className="text-red-600">Master Challenge - Landing Page</h1>
+      <h1 className='text-red-600'>Master Challenge - Landing Page</h1>
       <ChallengeDescription />
     </div>
   );
