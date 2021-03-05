6ef90f20832b7db394310fc8c02501e1097f0cdb
Mario Silva <hmariosilvae@gmail.com>
2021-03-05 12:54:22 -0600

    Add Input done

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,10 @@
+import  Input  from "../components/Input"
+
 const Home = () => {
   return (
     <div className="flex justify-center">
       <h1 className="text-red-600">Home - Menu Challenges</h1>
+      <Input />
     </div>);
 }
 
