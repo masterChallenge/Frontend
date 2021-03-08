8f9c363793db27c9b123caaea80098c97378c1f6
Djkde <sergio.estrella@utp.edu.co>
2021-03-08 15:27:27 -0500

    [FEATURE] Add Challenge Tips component

--- components/atoms/Button/Button.js
+++ components/atoms/Button/Button.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Button = ({ children, color }) => {
   return (
     <button
-      className={`flex rounded-2xl h-9 text-light shadow font-normal text-lg py-2 px-10 items-center bg-${color}`}
-      my-auto
+      className={`flex rounded-2xl h-9 text-light shadow font-normal text-lg py-2 px-10 items-center bg-${color} my-auto`}
     >
       {children}
     </button>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import ChallengeDescription from '@components/molecules/ChallengeDescription/ChallengeDescription';
-import Navbar from '@components/molecules/Navbar/Navbar';
+import ChallengeDescription from "@components/molecules/ChallengeDescription/ChallengeDescription";
+import ChallengeTips from "@components/molecules/ChallengeTips/ChallengeTips";
+import Navbar from "@components/molecules/Navbar/Navbar";
 
 const Landing = () => {
   return (
-    <div className='flex justify-center flex-wrap'>
+    <div className="flex justify-center flex-wrap">
       <Navbar />
-      <h1 className='text-red-600'>Master Challenge - Landing Page</h1>
+      <h1 className="text-red-600">Master Challenge - Landing Page</h1>
       <ChallengeDescription />
+      <ChallengeTips />
     </div>
   );
 };
