3eb584ac4c212721d98b7cb2c5e69ca39b0051fa
Juan Camilo Moreno <juancamilomg21@gmail.com>
2021-03-09 13:33:36 -0500

    Add Challenge Card done

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import Navbar from "@components/molecules/Navbar/Navbar";
+import ChallengeCard from '@components/molecules/ChallengeCard/ChallengeCard';
+import Navbar from '@components/molecules/Navbar/Navbar';
 
 const Landing = () => {
   return (
-    <div className="flex justify-center flex-wrap">
+    <div className='flex justify-center flex-wrap'>
       <Navbar />
-      <h1 className="text-green-400">Master Challenge - Landing Page</h1>
+      <h1 className='text-green-400'>Master Challenge - Landing Page</h1>
+      <ChallengeCard estatus='completed' />
     </div>
   );
 };
 
 export default Landing;
-
