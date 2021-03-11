cb6b850200f8b7fce8935df54229609af1389345
Raul Romo <rromomxdev@gmail.com>
2021-03-10 20:42:47 -0600

    Add api integration

--- components/molecules/Output/Output.jsx
+++ components/molecules/Output/Output.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
-export default function Output({ cssInputTest }) {
-
-  const htmlTest = ` 
-  <div class="main">
-    <div class="challenge">
-      <p>Hola</p>
-    </div>
-  </div>
-  `;
+export default function Output({ 
+  cssInputTest, 
+  htmlTest,
+  outCssTest,
+  outHtmlTest
+}) {
 
   function createMarkup() {
     const htmlCSS = `<style> ${cssInputTest} </style> ${htmlTest}`
+
+    return {__html: htmlCSS};
+  }
+
+  function finalMarkup() {
+    const htmlCSS = `<style> ${outCssTest} </style> ${outHtmlTest}`
 
     return {__html: htmlCSS};
   }
@@ -21,8 +24,8 @@ export default function Output({ cssInputTest }) {
     <div className="bg-gray-200 w-1/4"
       dangerouslySetInnerHTML={createMarkup()}>
     </div>
-    <div className="bg-gray-500 w-1/4">
-      <h2 className="text-gray-100">Expected output</h2>
+    <div className="bg-gray-200 w-1/4"
+      dangerouslySetInnerHTML={finalMarkup()}>
     </div>
     </>
   )
