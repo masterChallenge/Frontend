03ab8ca1184c8ec592a3cc4c741e9f66d90deb96
Juan Camilo Moreno <juancamilomg21@gmail.com>
2021-03-11 19:11:35 -0500

    Add Landing Page done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
+import React from 'react';
+import Button from '@components/atoms/Button/Button';
 import Navbar from '@components/molecules/Navbar/Navbar';
+import LandingImage from '@assets/Landing-image.svg';
+import { useRouter } from 'next/router';
 
 const Landing = () => {
+  const router = useRouter();
+
+  const handleRedirect = (e) => {
+    e.preventDefault();
+    router.push('/challenge/1');
+  };
+
   return (
-    <div className='flex justify-center flex-wrap'>
+    <>
       <Navbar />
-      <h1 className='text-green-400'>Master Challenge - Landing Page</h1>
-    </div>
+      <div className='fixed flex w-full pl-10 pt-20 bg-primary'>
+        <div>
+          <h1 className='text-6xl font-bold w-3/6 inline text-primary-lightest'>
+            Welcome to <br /> Master Challenges!
+          </h1>
+          <p className='mb-20 text-lg  mt-6 text-primary-lightest'>
+            A platform to improve your CSS Skills
+          </p>
+          <Button color='secondary' onClick={handleRedirect}>
+            See challenges
+          </Button>
+        </div>
+        <div className='w-3/6'>
+          <LandingImage className='ml-28' />
+        </div>
+      </div>
+    </>
   );
 };
 
