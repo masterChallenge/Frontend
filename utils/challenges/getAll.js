170b9408bdc6e81543f6b187ea5e1fd6fe4a3e3d
Raul Romo <rromomxdev@gmail.com>
2021-03-18 07:35:33 -0600

    fixed challenges api

--- utils/challenges/getAll.js
+++ utils/challenges/getAll.js
@@ -1,28 +1,7 @@
-
-
-const getAllChallenges = () => {
-  const fs = require('fs');
-  const dir = './jsons/files';
-  let challenges = {}
-  challenges['data'] = []
-  
-  fs.readdirSync(dir).forEach((file) => {
-    let contents = fs.readFileSync(`${dir}/${file}`, 'utf8')
-
-    const dataFromFile = JSON.parse(contents)
-
-    challenges.data.push({
-      id: dataFromFile.id,
-      name: dataFromFile.name || 'challenge',
-      difficulty: dataFromFile.difficulty,
-      image: dataFromFile.challenge.image,
-      attempts: 0,
-      estatus: 'incomplete'
-    })
-    
-  })
-
-  return challenges;
+const getAllChallenges = async () => {
+  const json = await import(`./challenges.json`)
+  console.log(json)
+  return json.default;
 }
 
 export { getAllChallenges }
