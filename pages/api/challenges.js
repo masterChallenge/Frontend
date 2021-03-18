42d7ebb04b56f354a63f0bd50f1cb936e0a450b6
Raul Romo <rromomxdev@gmail.com>
2021-03-18 09:31:27 -0600

    api json response

--- pages/api/challenges.js
+++ pages/api/challenges.js
@@ -2,5 +2,6 @@ import { getAllChallenges } from '../../utils/challenges/getAll'
 
 export default async function handler(req, res) {
   const challenges = await getAllChallenges()
-  res.end(JSON.stringify(challenges))
+  //res.end(JSON.stringify(challenges))
+  res.status(200).json(challenges)
 }
